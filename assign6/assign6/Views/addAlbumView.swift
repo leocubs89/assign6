9f2d504d5a052b6bb9e354e2dabbf41051ad1b70
Blue1818 <iwadale@gmail.com>
2022-05-05 20:07:05 -0500

    Update addAlbumView.swift

--- assign6/assign6/Views/addAlbumView.swift
+++ assign6/assign6/Views/addAlbumView.swift
@@ -13,50 +13,99 @@ struct addAlbumView: View {
     @State private var inputTitle = ""
     @State private var inputYear = ""
     @State private var selectedArtist: Artist? = nil
+
+    @State private var image: UIImage?
+    @State private var showPhotoLibrary = false
+    @State private var showAlert = false
+    @State private var errorMessage = ""
+
     
     var body: some View {
-        VStack(spacing: 12) {
-            HStack {
-                Text("Title:")
-                TextField("Insert Title", text: $inputTitle)
-                    .textFieldStyle(.roundedBorder)
-            }
-            
-            HStack {
-                Text("Year:")
-                TextField("Insert Year", text: $inputYear)
-                    .textFieldStyle(.roundedBorder)
-            }
-            
-            HStack(alignment: .top, spacing: 8) {
-                Text("Artist:")
-                VStack(alignment: .leading, spacing: 8) {
-                    Text(selectedArtist?.name ?? "Undefined")
-                        .foregroundColor(selectedArtist != nil ? Color.black : Color.gray)
-                    NavigationLink(destination: ArtistsView(selected: $selectedArtist), label: {
-                        Text("Select Artist")
-                    })
+        NavigationView {
+            VStack(spacing: 12) {
+                HStack {
+                    Text("Title:")
+                    TextField("Insert Title", text: $inputTitle)
+                        .textFieldStyle(.roundedBorder)
                 }
+                
+                HStack {
+                    Text("Year:")
+                    TextField("Insert Year", text: $inputYear)
+                        .textFieldStyle(.roundedBorder)
+                }
+                
+                HStack(alignment: .top, spacing: 8) {
+                    Text("Artist:")
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text(selectedArtist?.name ?? "Undefined")
+                            .foregroundColor(selectedArtist != nil ? Color.black : Color.gray)
+                        NavigationLink(destination: ArtistsView(selected: $selectedArtist), label: {
+                            Text("Select Artist")
+                        })
+                    }
+                }
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                Spacer()
+
+                if (image == nil) {
+                    Image(uiImage: UIImage())
+                        .resizable()
+                        .scaledToFit()
+                        .padding(.horizontal)
+                } else {
+                    Image(uiImage: image!)
+                        .resizable()
+                        .scaledToFit()
+                        .padding(.horizontal)
+                }
+                
+                Button(action: {
+                    showPhotoLibrary = true
+                }, label: {
+                    HStack {
+                        Image(systemName: "photo")
+                            .font(.system(size: 20))
+                        
+                        Text("Photo Library")
+                            .font(.headline)
+                    }
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 50)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(20)
+                    .padding(.horizontal)
+                })
+
+
+                Spacer()
             }
-            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-            Spacer()
-        }
-        .padding()
-        .navigationBarTitle("Add Album")
-        .toolbar {
-            ToolbarItem(placement: .navigationBarTrailing) {
-                Button("Save") {
-                    let newTitle = inputTitle.trimmingCharacters(in: .whitespaces)
-                    let year = Int32(inputYear)
-                    if !newTitle.isEmpty && year != nil {
-                        Task(priority: .high) {
-                            await storeAlbum(title: newTitle, year: year!)
+            .padding()
+            .navigationBarTitle("Add Album")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Save") {
+                        let newTitle = inputTitle.trimmingCharacters(in: .whitespaces)
+                        let year = Int32(inputYear)
+                        if !newTitle.isEmpty && year != nil {
+                            Task(priority: .high) {
+                                await storeAlbum(title: newTitle, year: year!)
+                            }
                         }
                     }
                 }
             }
+        }   //navigationview
+        .sheet(isPresented: $showPhotoLibrary) {
+            ImagePicker(sourceType: .photoLibrary, selectedImage: $image)
         }
-    }
+        .alert(isPresented: $showAlert) {
+            Alert(title: Text("Error"), message: Text(errorMessage))
+        }
+
+
+
+    } //body
     
     func storeAlbum(title: String, year: Int32) async {
         await dbContext.perform {
